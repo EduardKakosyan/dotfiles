3a989ebe2ea619628e3bc1450af08d7852e114be
Eduard Kakosyan <90533005+EduardKakosyan@users.noreply.github.com>
2024-12-07 19:53:41 -0400

    [Fix] Fixed my dumb ass way of doing dotfiles

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,91 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+local Util = require("lazyvim.util")
+
+-- Github Copilot
+keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { silent = true })
+keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
+keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { silent = true })
+keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { silent = true })
+keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { silent = true })
+keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { silent = true })
+
+-- Borderless terminal
+vim.keymap.set("n", "<C-/>", function()
+  Util.terminal(nil, { border = "none" })
+end, { desc = "Term with border" })
+
+-- Borderless lazygit
+vim.keymap.set("n", "<leader>gg", function()
+  Util.terminal({ "lazygit" }, { cwd = Util.root(), esc_esc = false, ctrl_hjkl = false, border = "none" })
+end, { desc = "Lazygit (root dir)" })
+
+keymap.del({ "n", "i", "v" }, "<A-j>")
+keymap.del({ "n", "i", "v" }, "<A-k>")
+keymap.del("n", "<C-Left>")
+keymap.del("n", "<C-Down>")
+keymap.del("n", "<C-Up>")
+keymap.del("n", "<C-Right>")
+
+keymap.set("n", "<M-h>", '<Cmd>lua require("tmux").resize_left()<CR>', { silent = true })
+keymap.set("n", "<M-j>", '<Cmd>lua require("tmux").resize_bottom()<CR>', { silent = true })
+keymap.set("n", "<M-k>", '<Cmd>lua require("tmux").resize_top()<CR>', { silent = true })
+keymap.set("n", "<M-l>", '<Cmd>lua require("tmux").resize_right()<CR>', { silent = true })
+
+local set_keymap = vim.api.nvim_set_keymap
+
+keymap.set("n", "<C-a>", "ggVG")
+
+-- Split windows
+keymap.set("n", "ss", ":vsplit<Return>", opts)
+keymap.set("n", "sv", ":split<Return>", opts)
+
+-- Tabs
+keymap.set("n", "te", ":tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- package-info keymaps
+set_keymap(
+  "n",
+  "<leader>cpt",
+  "<cmd>lua require('package-info').toggle()<cr>",
+  { silent = true, noremap = true, desc = "Toggle" }
+)
+set_keymap(
+  "n",
+  "<leader>cpd",
+  "<cmd>lua require('package-info').delete()<cr>",
+  { silent = true, noremap = true, desc = "Delete package" }
+)
+set_keymap(
+  "n",
+  "<leader>cpu",
+  "<cmd>lua require('package-info').update()<cr>",
+  { silent = true, noremap = true, desc = "Update package" }
+)
+set_keymap(
+  "n",
+  "<leader>cpi",
+  "<cmd>lua require('package-info').install()<cr>",
+  { silent = true, noremap = true, desc = "Install package" }
+)
+set_keymap(
+  "n",
+  "<leader>cpc",
+  "<cmd>lua require('package-info').change_version()<cr>",
+  { silent = true, noremap = true, desc = "Change package version" }
+)
+
+--Save with escape key
+keymap.set("n", "<esc>", ":w<CR>", opts)
+
+keymap.set("n", "<leader>rw", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })<CR>", opts)
+keymap.set("n", "<leader>rW", ":lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })<CR>", opts)
+keymap.set("n", "<leader>re", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })<CR>", opts)
+keymap.set("n", "<leader>ra", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.HINT })<CR>", opts)
+keymap.set("n", "<leader>rA", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.HINT })<CR>", opts)
+keymap.set("n", "<leader>rs", ":lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.INFO })<CR>", opts)
