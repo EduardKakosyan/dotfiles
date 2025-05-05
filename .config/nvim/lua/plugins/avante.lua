8e6d7d83aae923188d08f0dbe4a6a3f777ea0d2d
Eduard Kakosyan <kakosyaneduard@dal.ca>
2025-05-04 21:38:44 -0300

    feat: ram usage added to bar

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -3,16 +3,9 @@ return {
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
   opts = {
-    -- add any opts here
-    -- for example
-    provider = "copilot",
-    openai = {
-      endpoint = "https://api.openai.com/v1",
-      model = "o4-mini", -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-      temperature = 0,
-      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    provider = "ollama",
+    ollama = {
+      model = "qwen3:8b",
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -23,7 +16,6 @@ return {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
     "echasnovski/mini.pick", -- for file_selector provider mini.pick
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
--- .raycast/scripts/open-cursor-on-aerospace-2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# this is a raycast script to open cursor on aerospace
-# Open Cursor on Aerospace
-
